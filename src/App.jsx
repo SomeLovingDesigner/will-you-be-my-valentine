344a2ed0908d853251c7a53bdc592e34cf92c9a0
SomeLovingDesigner <bcarvlho2056@gmail.com>
2025-01-16 22:20:56 -1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -45,13 +45,37 @@ function App() {
   function getNoButtonText() {
     return phrases[Math.min(noCount, phrases.length - 1)];
   }
+  
+  // Function for fun button to work <3
+    function handleFunButtonClick() {
+    setFunActionTaken(true);
+  }
 
+  
   return (
     <div className='valentine-container'>
       {yesPressed ? (  // Display content after clicking the "YES" button
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="bear-Kissing" />
           <div className='text'>YAYYYYY&lt;33</div>
+          {!funActionTaken ? ( //Start
+            <button
+              className='funButton'
+              style={{
+                backgroundColor: 'rgb(255, 182, 193)',
+                padding: '0.8em 1.5em',
+                fontSize: '1.2em',
+                margin: '1rem',
+                borderRadius: '10px',
+                fontWeight: '600'
+              }}
+              onClick={handleFunButtonClick}
+            >
+              Do something fun!
+            </button>
+          ) : (
+            <div className='text'>Thanks for joining the fun! 🥳</div>
+          )} //End
         </>
       ) : ( // Display content before clicking the "YES" button
         <>
