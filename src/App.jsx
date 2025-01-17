8967967e90b970be9279a86db5a4ca25b8bf31d8
SomeLovingDesigner <bcarvlho2056@gmail.com>
2025-01-16 23:11:20 -1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './index.css';
 
-// Array of phrases to be displayed when the "NO" button is clicked
 const phrases = [
   "No",
   "why 8(",
@@ -27,40 +26,42 @@ const phrases = [
 ]
 
 function App() {
-  const [noCount, setNoCount] = useState(0); // State to keep track of the number of clicks on the "NO" button
-  const [yesPressed, setYesPressed] = useState(false); // State to check if the "YES" button has been clicked
-  const yesButtonSize = noCount * 20 + 16; // Dynamic adjustment of the "YES" button size based on the number of clicks on the "NO" button
-  const [showSecondButton, setShowSecondButton] = useState(false); // State to show the second button
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const [showSecondButton, setShowSecondButton] = useState(false);
+  const [shakeIntensity, setShakeIntensity] = useState('');
 
-  // Function to handle the click on the "YES" button
+  const yesButtonSize = noCount * 20 + 16;
+
+  // Handle "YES" button click
   function handleYesClick() {
-    setYesPressed(true)
-    setTimeout(() => setShowSecondButton(true), 1350); // Show the second button after a delay
+    setYesPressed(true);
+    setTimeout(() => setShowSecondButton(true), 1350);
   }
 
-  // Function to handle the click on the "NO" button
+  // Handle "NO" button click with screen shake
   function handleNoClick() {
-    setNoCount(noCount+1);
+    setNoCount(noCount + 1);
+
+    // Determine shake intensity based on noCount
+    const intensity = noCount < 5 ? 'shake' : 'shake-intense';
+    setShakeIntensity(intensity);
+
+    // Remove shake class after animation
+    setTimeout(() => setShakeIntensity(''), 1000);
   }
 
-  // Function to get the text for the "NO" button based on the current number of clicks
   function getNoButtonText() {
     return phrases[Math.min(noCount, phrases.length - 1)];
   }
-  
-  // Function for fun button to work <3
-    function handleFunButtonClick() {
-    setFunActionTaken(true);
-  }
 
-  
   return (
-    <div className='valentine-container'>
-      {yesPressed ? (  // Display content after clicking the "YES" button
+    <div className={`valentine-container ${shakeIntensity}`}>
+      {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="bear-Kissing" />
           <div className='text'>YAYYYYY&lt;33</div>
-           {showSecondButton && ( // Show the second button after a delay
+          {showSecondButton && (
             <button
               className='funButton'
               style={{ fontSize: '1.5em', marginTop: '10px' }}
@@ -70,20 +71,20 @@ function App() {
             </button>
           )}
         </>
-      ) : ( // Display content before clicking the "YES" button
+      ) : (
         <>
           <img className='kiss' src="https://i.pinimg.com/originals/73/89/aa/7389aaa2a73b071ee7d3d782310e590b.gif" alt="bear with heart" />
           <div className='text'>Will you be my valentine, Tiares?🌹</div>
           <div className='both-Button'>
-            <button className='yesButton'
-            style={{ fontSize: yesButtonSize , backgroundColor:'rgb(144, 238, 144)'}}
+            <button
+              className='yesButton'
+              style={{ fontSize: yesButtonSize, backgroundColor: 'rgb(144, 238, 144)' }}
               onClick={handleYesClick}
             >
               Yes&lt;3
             </button>
             <button
               className='NoButton'
-              style={{ }}
               onClick={handleNoClick}
             >
               {getNoButtonText()}
